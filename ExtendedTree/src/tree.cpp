604840b7fc7cd904c784883c2f4ba378c39b61d1
dsw7 <dds3443@hotmail.com>
2025-01-11 00:07:59 -0800

    Add iterator func

--- ExtendedTree/src/tree.cpp
+++ ExtendedTree/src/tree.cpp
@@ -3,9 +3,33 @@
 #include <filesystem>
 #include <fmt/core.h>
 #include <iostream>
+#include <map>
 #include <stdexcept>
+#include <string>
 
 namespace fs = std::filesystem;
+
+namespace {
+
+void iterate_over_dirs(const fs::path &target)
+{
+    static std::map<int, std::string> ws = {};
+    static int tw = 4;
+    int depth = 0;
+
+    for (auto it = fs::recursive_directory_iterator(target); it != fs::recursive_directory_iterator(); ++it) {
+        depth = it.depth();
+
+        if (!ws.contains(depth)) {
+            ws[depth] = std::string(depth * tw, ' ');
+        }
+
+        std::string t = it->path();
+        std::cout << ws[depth] << t << '\n';
+    }
+}
+
+} // namespace
 
 void run_tree(const Params &params)
 {
@@ -25,7 +49,5 @@ void run_tree(const Params &params)
         throw std::runtime_error(fmt::format("'{}' is not a directory", target.string()));
     }
 
-    for (auto it = fs::recursive_directory_iterator(target); it != fs::recursive_directory_iterator(); ++it) {
-        std::cout << it.depth() << ' ' << *it << '\n';
-    }
+    iterate_over_dirs(target);
 }
