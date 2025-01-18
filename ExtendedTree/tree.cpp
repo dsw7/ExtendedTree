a3be75de6fc872349d2554e44402ec1df75bf779
dsw7 <dds3443@hotmail.com>
2025-01-18 01:47:27 -0800

    Migrate working tree builder

--- ExtendedTree/tree.cpp
+++ ExtendedTree/tree.cpp
@@ -4,8 +4,10 @@
 #include <fmt/color.h>
 #include <fmt/core.h>
 #include <map>
+#include <memory>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 namespace fs = std::filesystem;
 
@@ -16,7 +18,56 @@ const std::string hline = "─";
 const std::string tee = "├";
 const std::string vline = "│";
 
-std::map<int, std::string> ws;
+enum FileType {
+    REGULAR_FILE,
+    DIRECTORY,
+    OTHER,
+};
+
+struct FileNode {
+    FileType filetype = REGULAR_FILE;
+    std::string filename;
+    std::vector<std::unique_ptr<FileNode>> children;
+
+    FileNode(const std::string &filename, const FileType filetype)
+    {
+        this->filename = filename;
+        this->filetype = filetype;
+    }
+};
+
+void print(int depth, const std::unique_ptr<FileNode> &node)
+{
+    static std::map<int, std::string> ws;
+
+    if (!ws.contains(depth)) {
+        ws.emplace(depth, std::string(depth * 4, ' '));
+    }
+
+    switch (node->filetype) {
+        case REGULAR_FILE:
+            fmt::print("{}{}\n", ws[depth], node->filename);
+            break;
+        case DIRECTORY:
+            fmt::print(fg(fmt::terminal_color::bright_blue), "{}{}/\n", ws[depth], node->filename);
+            break;
+        case OTHER:
+            fmt::print(fg(fmt::terminal_color::bright_green), "{}{} ?\n", ws[depth], node->filename);
+            break;
+        default:
+            fmt::print(fg(fmt::terminal_color::bright_green), "{}{} ?\n", ws[depth], node->filename);
+    }
+}
+
+void traverse(const std::unique_ptr<FileNode> &node, int depth = 0)
+{
+    print(depth, node);
+    depth++;
+
+    for (const auto &child: node->children) {
+        traverse(child, depth);
+    }
+}
 
 struct Stats {
     int num_directories = 0;
@@ -25,50 +76,31 @@ struct Stats {
     uintmax_t total_size = 0;
 };
 
-void insert_new_ws(int depth)
+void iterate_over_dirs(const std::string &dir, FileNode &parent)
 {
-    if (!ws.contains(depth)) {
-        ws.emplace(depth, std::string(depth * 4, ' '));
-    }
-}
-
-void print_dir(int depth, const fs::directory_entry &dir, uintmax_t dir_size)
-{
-    fmt::print(fg(fmt::terminal_color::bright_blue), "{}{}/ ", ws[depth], dir.path().filename().string());
-    fmt::print(fg(fmt::terminal_color::bright_green), "{}\n", dir_size);
-}
-
-void print_file(int depth, const fs::directory_entry &file, uintmax_t file_size)
-{
-    fmt::print("{}{} {}\n", ws[depth], file.path().filename().string(), file_size);
-}
-
-uintmax_t iterate_over_dirs(const std::string &dir, Stats &stats, int depth)
-{
-    insert_new_ws(depth);
-
-    uintmax_t dir_total_size = 0;
-
     for (auto const &entry: fs::directory_iterator { dir }) {
+        FileType filetype;
 
-        if (entry.is_directory()) {
-            stats.num_directories++;
-            uintmax_t dir_size = iterate_over_dirs(entry.path().string(), stats, depth + 1);
-            print_dir(depth, entry, dir_size);
-            dir_total_size += dir_size;
-        } else if (entry.is_regular_file()) {
-            stats.num_files++;
-            uintmax_t file_size = fs::file_size(entry);
-            print_file(depth, entry, file_size);
-            dir_total_size += file_size;
+        if (entry.is_regular_file()) {
+            filetype = REGULAR_FILE;
+        } else if (entry.is_directory()) {
+            filetype = DIRECTORY;
         } else {
-            stats.num_other++;
+            filetype = OTHER;
         }
-    }
 
-    return dir_total_size;
+        std::string filename = entry.path().filename();
+        std::unique_ptr<FileNode> child = std::make_unique<FileNode>(filename, filetype);
+
+        if (filetype == DIRECTORY) {
+            iterate_over_dirs(entry.path().string(), *child);
+        }
+
+        parent.children.push_back(std::move(child));
+    }
 }
 
+/*
 void print_report(const Stats &stats)
 {
     fmt::print("\nTotal size: {} bytes\n", stats.total_size);
@@ -76,6 +108,7 @@ void print_report(const Stats &stats)
     fmt::print("Number of files: {}\n", stats.num_files);
     fmt::print("Number of other file-like objects: {}\n", stats.num_other);
 }
+*/
 
 } // namespace
 
@@ -97,12 +130,16 @@ void run_tree(const Params &params)
         throw std::runtime_error(fmt::format("'{}' is not a directory", target.string()));
     }
 
-    fmt::print(fg(fmt::terminal_color::bright_blue), "{}/\n", target.string());
+    auto root = std::make_unique<FileNode>(target.string(), DIRECTORY);
+    iterate_over_dirs(target.string(), *root);
+    traverse(root);
 
+    /*
     Stats stats;
     int start_depth = 1;
     uintmax_t total_size = iterate_over_dirs(target, stats, start_depth);
     stats.total_size = total_size;
+    */
 
-    print_report(stats);
+    // print_report(stats);
 }
