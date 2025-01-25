e33aa75c8538af3fd6169537f5354e46f130cad3
dsw7 <dds3443@hotmail.com>
2025-01-25 07:03:31 -0800

    Assert sizes

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,9 +1,19 @@
+from json import loads
+from typing import TypedDict, TypeAlias, Iterable
 from os import environ
 from pathlib import Path
 from shutil import rmtree
-from subprocess import call
+from subprocess import run, PIPE
 from tempfile import gettempdir
 from unittest import TestCase
+
+Layers: TypeAlias = dict[int, list[int]]
+
+
+class Tree(TypedDict):
+    filename: str
+    filesize: int
+    children: Iterable["Tree"]
 
 
 def build_test_dir() -> Path:
@@ -31,7 +41,19 @@ def build_test_dir() -> Path:
     return test_dir
 
 
-class TestSimpleMath(TestCase):
+def traverse_level_order(stdout: Tree, layers: Layers, depth: int = 0) -> None:
+    if depth not in layers:
+        layers[depth] = []
+
+    layers[depth].append(stdout["filesize"])
+
+    depth += 1
+
+    for child in stdout["children"]:
+        traverse_level_order(child, layers, depth)
+
+
+class TestAbsolute(TestCase):
 
     def setUp(self) -> None:
         self.test_dir: Path = build_test_dir()
@@ -42,5 +64,13 @@ class TestSimpleMath(TestCase):
             rmtree(self.test_dir)
 
     def test_basic(self) -> None:
-        exit_code = call([self.bin, self.test_dir, "-j -1"])
-        self.assertEqual(exit_code, 0)
+        process = run([self.bin, self.test_dir, "-j -1"], stdout=PIPE)
+        self.assertEqual(process.returncode, 0)
+
+        stdout: Tree = loads(process.stdout.decode())
+        layers: Layers = {}
+        traverse_level_order(stdout, layers)
+
+        self.assertListEqual(layers[0], [27])
+        self.assertListEqual(layers[1], [9, 9, 9])
+        self.assertListEqual(layers[2], [3, 3, 3, 3, 3, 3, 3, 3, 3])
