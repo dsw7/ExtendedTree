aacb7f2a0111eb8963eab2b0abda6fbc8fa049f0
dsw7 <dds3443@hotmail.com>
2025-01-25 03:08:35 -0800

    Getter and setter system

--- ExtendedTree/filenode.cpp
+++ ExtendedTree/filenode.cpp
@@ -6,3 +6,33 @@ FileNode::FileNode(const std::string &filename, const FileType filetype, const s
     this->filetype = filetype;
     this->filesize = filesize;
 }
+
+void FileNode::set_is_file()
+{
+    this->type = REGULAR_FILE;
+}
+
+void FileNode::set_is_directory()
+{
+    this->type = DIRECTORY;
+}
+
+void FileNode::set_is_other()
+{
+    this->type = OTHER;
+}
+
+bool FileNode::is_file()
+{
+    return this->type == REGULAR_FILE;
+}
+
+bool FileNode::is_directory()
+{
+    return this->type == DIRECTORY;
+}
+
+bool FileNode::is_other()
+{
+    return this->type == OTHER;
+}
--- ExtendedTree/filenode.hpp
+++ ExtendedTree/filenode.hpp
@@ -11,7 +11,11 @@ enum FileType {
     OTHER,
 };
 
-struct FileNode {
+class FileNode {
+private:
+    FileType type = REGULAR_FILE;
+
+public:
     FileType filetype = REGULAR_FILE;
 
     std::optional<uintmax_t> filesize = std::nullopt;
@@ -19,4 +23,11 @@ struct FileNode {
     std::vector<std::unique_ptr<FileNode>> children;
 
     FileNode(const std::string &filename, const FileType filetype, const std::optional<uintmax_t> &filesize);
+
+    void set_is_file();
+    void set_is_directory();
+    void set_is_other();
+    bool is_file();
+    bool is_directory();
+    bool is_other();
 };
