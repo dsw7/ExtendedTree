097a23d9c0a4c73830711c0c8a8b030b6609e192
dsw7 <dds3443@hotmail.com>
2025-01-26 02:53:30 -0800

    SoC

---
+++ tests/base.py
@@ -0,0 +1,43 @@
+from os import environ
+from pathlib import Path
+from shutil import rmtree
+from tempfile import gettempdir
+from unittest import TestCase
+
+
+def build_test_dir() -> Path:
+    test_dir = Path(gettempdir()) / "etree_test"
+    test_dir.mkdir(exist_ok=True)
+
+    dir_a = test_dir / "foo"
+    dir_b = test_dir / "bar"
+    dir_c = test_dir / "baz"
+
+    dir_a.mkdir(exist_ok=True)
+    dir_b.mkdir(exist_ok=True)
+    dir_c.mkdir(exist_ok=True)
+
+    (dir_a / "a.txt").write_text("abc")
+    (dir_a / "b.txt").write_text("abc")
+    (dir_a / "c.txt").write_text("abc")
+    (dir_b / "a.txt").write_text("abc")
+    (dir_b / "b.txt").write_text("abc")
+    (dir_b / "c.txt").write_text("abc")
+    (dir_c / "a.txt").write_text("abc")
+    (dir_c / "b.txt").write_text("abc")
+    (dir_c / "c.txt").write_text("abc")
+
+    return test_dir
+
+
+class TestTree(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.test_dir: Path = build_test_dir()
+        cls.bin: str = environ["PATH_BIN"]
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        if cls.test_dir.exists():
+            rmtree(cls.test_dir)
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,11 +1,7 @@
 from json import loads
 from typing import TypedDict, TypeAlias, Iterable
-from os import environ
-from pathlib import Path
-from shutil import rmtree
 from subprocess import run, PIPE, DEVNULL
-from tempfile import gettempdir
-from unittest import TestCase
+from .base import TestTree
 
 Layers: TypeAlias = dict[int, list[int]]
 
@@ -14,31 +10,6 @@ class Tree(TypedDict):
     filename: str
     filesize: int
     children: Iterable["Tree"]
-
-
-def build_test_dir() -> Path:
-    test_dir = Path(gettempdir()) / "etree_test"
-    test_dir.mkdir(exist_ok=True)
-
-    dir_a = test_dir / "foo"
-    dir_b = test_dir / "bar"
-    dir_c = test_dir / "baz"
-
-    dir_a.mkdir(exist_ok=True)
-    dir_b.mkdir(exist_ok=True)
-    dir_c.mkdir(exist_ok=True)
-
-    (dir_a / "a.txt").write_text("abc")
-    (dir_a / "b.txt").write_text("abc")
-    (dir_a / "c.txt").write_text("abc")
-    (dir_b / "a.txt").write_text("abc")
-    (dir_b / "b.txt").write_text("abc")
-    (dir_b / "c.txt").write_text("abc")
-    (dir_c / "a.txt").write_text("abc")
-    (dir_c / "b.txt").write_text("abc")
-    (dir_c / "c.txt").write_text("abc")
-
-    return test_dir
 
 
 def traverse_level_order(stdout: Tree, layers: Layers, depth: int = 0) -> None:
@@ -51,19 +22,6 @@ def traverse_level_order(stdout: Tree, layers: Layers, depth: int = 0) -> None:
 
     for child in stdout["children"]:
         traverse_level_order(child, layers, depth)
-
-
-class TestTree(TestCase):
-
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.test_dir: Path = build_test_dir()
-        cls.bin: str = environ["PATH_BIN"]
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        if cls.test_dir.exists():
-            rmtree(cls.test_dir)
 
 
 class TestCommandLine(TestTree):
