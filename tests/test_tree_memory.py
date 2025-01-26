9295e9b0c05ab2643bf3c831fa74d5d3521f8b04
dsw7 <dds3443@hotmail.com>
2025-01-26 03:31:42 -0800

    Add valgrind tests

---
+++ tests/test_tree_memory.py
@@ -0,0 +1,42 @@
+from subprocess import run, DEVNULL, PIPE
+from .base import TestTree
+
+
+def run_valgrind(args: list[str]) -> None:
+    root = ["valgrind", "--error-exitcode=2", "--leak-check=full"]
+
+    process = run(root + args, stdout=DEVNULL, stderr=PIPE)
+    stderr = process.stderr.decode()
+
+    if process.returncode == 2:
+        assert False, f"Found a memory leak\n\n{stderr}"
+
+
+class TestMemory(TestTree):
+
+    def test_help(self) -> None:
+        run_valgrind([self.bin, "-h"])
+
+    def test_missing_dir(self) -> None:
+        run_valgrind([self.bin, "/foobar"])
+
+    def test_traverse_file(self) -> None:
+        run_valgrind([self.bin, self.test_dir / "foo" / "a.txt"])
+
+    def test_relative(self) -> None:
+        run_valgrind([self.bin, self.test_dir])
+
+    def test_absolute(self) -> None:
+        run_valgrind([self.bin, self.test_dir, "-a"])
+
+    def test_json_absolute(self) -> None:
+        run_valgrind([self.bin, self.test_dir, "-a", "-j -1"])
+
+    def test_json_relative(self) -> None:
+        run_valgrind([self.bin, self.test_dir, "-j -1"])
+
+    def test_dirs_only_absolute(self) -> None:
+        run_valgrind([self.bin, self.test_dir, "-da"])
+
+    def test_dirs_only_relative(self) -> None:
+        run_valgrind([self.bin, self.test_dir, "-d"])
