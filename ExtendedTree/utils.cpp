c123a5f5843c82a61be78900fa205a04b4268983
dsw7 <dds3443@hotmail.com>
2025-02-10 02:06:10 -0800

    Add helper for converting to human readable

--- ExtendedTree/utils.cpp
+++ ExtendedTree/utils.cpp
@@ -1,5 +1,7 @@
 #include "utils.hpp"
 
+#include <cmath>
+
 void strip_extra_path_delimiter(std::string &path)
 {
     if (path.size() < 2) {
@@ -18,4 +20,30 @@ float compute_relative_usage(uintmax_t size, uintmax_t total_size)
     }
 
     return ((float)size / total_size) * 100;
+}
+
+std::string bytes_to_human(uintmax_t bytes)
+{
+    static uintmax_t kb = 1000;
+    static uintmax_t mb = std::pow(1000, 2);
+    static uintmax_t gb = std::pow(1000, 3);
+    static uintmax_t tb = std::pow(1000, 4);
+
+    if (bytes < kb) {
+        return std::to_string(bytes);
+    }
+
+    std::string result;
+
+    if (bytes >= kb && bytes < mb) {
+        result = std::to_string((bytes + kb - 1) / kb) + 'K';
+    } else if (bytes >= mb && bytes < gb) {
+        result = std::to_string((bytes + mb - 1) / mb) + 'M';
+    } else if (bytes >= gb && bytes < tb) {
+        result = std::to_string((bytes + gb - 1) / gb) + 'G';
+    } else {
+        result = std::to_string((bytes + tb - 1) / tb) + 'T';
+    }
+
+    return result;
 }
--- ExtendedTree/utils.hpp
+++ ExtendedTree/utils.hpp
@@ -6,3 +6,4 @@ const int TAB_WIDTH = 4;
 
 void strip_extra_path_delimiter(std::string &path);
 float compute_relative_usage(uintmax_t size, uintmax_t total_size);
+std::string bytes_to_human(uintmax_t bytes);
