ac9ddb15afdbdc18a4fb5eab68472ae2618f9114
dsw7 <dds3443@hotmail.com>
2025-02-10 01:17:31 -0800

    Swap order

--- ExtendedTree/utils.cpp
+++ ExtendedTree/utils.cpp
@@ -1,14 +1,5 @@
 #include "utils.hpp"
 
-float compute_relative_usage(uintmax_t size, uintmax_t total_size)
-{
-    if (total_size == 0) {
-        return 0;
-    }
-
-    return ((float)size / total_size) * 100;
-}
-
 void strip_extra_path_delimiter(std::string &path)
 {
     if (path.size() < 2) {
@@ -18,4 +9,13 @@ void strip_extra_path_delimiter(std::string &path)
     if (path.back() == '/') {
         path.pop_back();
     }
+}
+
+float compute_relative_usage(uintmax_t size, uintmax_t total_size)
+{
+    if (total_size == 0) {
+        return 0;
+    }
+
+    return ((float)size / total_size) * 100;
 }
--- ExtendedTree/utils.hpp
+++ ExtendedTree/utils.hpp
@@ -4,5 +4,5 @@
 
 const int TAB_WIDTH = 4;
 
-float compute_relative_usage(uintmax_t size, uintmax_t total_size);
 void strip_extra_path_delimiter(std::string &path);
+float compute_relative_usage(uintmax_t size, uintmax_t total_size);
