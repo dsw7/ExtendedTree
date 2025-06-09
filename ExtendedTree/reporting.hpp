9d1e3b106a4efb2ab71906315b3c53b7b21a4f26
dsw7 <dds3443@hotmail.com>
2025-06-09 00:41:19 -0700

    Working prototype

--- ExtendedTree/reporting.hpp
+++ ExtendedTree/reporting.hpp
@@ -6,7 +6,7 @@
 
 namespace reporting {
 
-void print_pretty_output(const std::unique_ptr<filenode::FileNode> &node, uintmax_t total_size, int depth = 0);
+void print_pretty_output(const std::unique_ptr<filenode::FileNode> &node, uintmax_t total_size, int depth = 0, const std::string &prefix = "", bool is_last = true);
 void print_pretty_output_dirs_only(const std::unique_ptr<filenode::FileNode> &node, uintmax_t total_size, int depth = 0);
 void print_json(const std::unique_ptr<filenode::FileNode> &node, uintmax_t total_size);
 
